ed0a03a6bcf5f9ffe1d9c697c508f994c894b926
vincent <18wjchen1@stu.edu.cn>
2021-04-28 10:26:28 +0800

    typescript,component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>Vue3</div>
+  <div>ROOT Page</div>
+  <Helloworld></Helloworld>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import Helloworld from "./components/HelloWorld.vue";
 export default defineComponent({
   name: "APP",
-  setup() {
+  setup() {},
+  components: {
+    Helloworld,
   },
 });
 </script>
 <style>
 div {
-  color: yellowgreen;
+  color: black;
+  font-size: 30px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,18 @@
 <template>
-
+  <div class="container">
+    {{ msg }}
+  </div>
 </template>
-<script>
-
-
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "helloworld",
+  setup(props) {
+    let msg = ref("helloworld");
+    return {
+      msg,
+    };
+  },
+});
 </script>
-<style>
-
-</style>/
+<style></style>
